441ba091a269abeabb2ff4eb621f98ed03b4ab4f
quincy wang <mex2000@163.com>
2021-02-01 17:16:00 +0800

    删除多余的注释

--- rock-comm/src/main/java/org/quincy/rock/comm/communicate/AbstractChannel.java
+++ rock-comm/src/main/java/org/quincy/rock/comm/communicate/AbstractChannel.java
@@ -4,22 +4,6 @@ import org.quincy.rock.core.util.CoreUtil;
 import org.quincy.rock.core.vo.Vo;
 
 /**
- * <b>自定义通道抽象基类。</b>
- * <p><b>详细说明：</b></p>
- * <!-- 在此添加详细说明 -->
- * 无。
- * <p><b>修改列表：</b></p>
- * <table width="100%" cellSpacing=1 cellPadding=3 border=1>
- * <tr bgcolor="#CCCCFF"><td>序号</td><td>作者</td><td>修改日期</td><td>修改内容</td></tr>
- * <!-- 在此添加修改列表，参考第一行内容 -->
- * <tr><td>1</td><td>wks</td><td>2018年6月1日 下午5:14:54</td><td>建立类型</td></tr>
- * 
- * </table>
- * @version 1.0
- * @author wks
- * @since 1.0
- */
-/**
  * <b>AbstractChannel。</b>
  * <p><b>详细说明：</b></p>
  * <!-- 在此添加详细说明 -->
