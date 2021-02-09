b6c845cfcdea59deeaa4aab10fdd323632403775
quincy wang <mex2000@163.com>
2021-02-09 14:13:02 +0800

    fix

--- rock-comm/src/main/java/org/quincy/rock/comm/communicate/AbstractCommunicator.java
+++ rock-comm/src/main/java/org/quincy/rock/comm/communicate/AbstractCommunicator.java
@@ -57,7 +57,7 @@ public abstract class AbstractCommunicator<UChannel> implements Communicator<UCh
 	 * 无。
 	 * @return 日志记录器
 	 */
-	public final Recorder getRecorder() {
+	public Recorder getRecorder() {
 		return recorder;
 	}
 
@@ -68,7 +68,7 @@ public abstract class AbstractCommunicator<UChannel> implements Communicator<UCh
 	 * 无。
 	 * @param recorder 日志记录器
 	 */
-	public final void setRecorder(Recorder recorder) {
+	public void setRecorder(Recorder recorder) {
 		this.recorder = recorder;
 	}
 
